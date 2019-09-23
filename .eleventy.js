3c25b22091cf0d926cac8a599ec562dcec8b6c2e
Arpit Batra <arpitbatra123@gmail.com>
2019-09-23 20:30:37 +0530

    Add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 module.exports = eleventyConfig => {
   eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPassthroughCopy('favicon.ico');
 };
