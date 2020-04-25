2187d7518061840279284e81b9b808258e390321
Arpit Batra <arpitbatra123@gmail.com>
2020-04-25 18:00:55 +0530

    adds support for lazy loading of images

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,23 @@
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight"),
-  markdownIt = require("markdown-it");
-module.exports = eleventyConfig => {
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight'),
+  markdownLazyLoadImages = require('markdown-it-image-lazy-loading'),
+  markdownIt = require('markdown-it');
+
+module.exports = (eleventyConfig) => {
   eleventyConfig.addPlugin(syntaxHighlight);
 
-  eleventyConfig.addPassthroughCopy("assets");
-  eleventyConfig.addPassthroughCopy("favicon.ico");
+  eleventyConfig.addPassthroughCopy('assets');
+  eleventyConfig.addPassthroughCopy('favicon.ico');
 
   eleventyConfig.setDataDeepMerge(true);
   const options = {
     html: true,
     breaks: true,
     linkify: false
-  };
+  },
+  markdownEngine = markdownIt(options);
+  markdownEngine.use(markdownLazyLoadImages);
 
-  eleventyConfig.setLibrary("md", markdownIt(options));
+  eleventyConfig.setLibrary('md', markdownEngine);
 
   // For extra config options
   // return {
