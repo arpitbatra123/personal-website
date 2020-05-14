51b929e08ab32b7bf7cd386d1844ef3c10851163
Arpit Batra <arpitbatra123@gmail.com>
2020-05-14 21:48:41 +0530

    Add rss feed (atom) to website

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight'),
   markdownLazyLoadImages = require('markdown-it-image-lazy-loading'),
-  markdownIt = require('markdown-it');
+  markdownIt = require('markdown-it'),
+  pluginRss = require('@11ty/eleventy-plugin-rss');
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(pluginRss);
 
   eleventyConfig.addPassthroughCopy('assets');
   eleventyConfig.addPassthroughCopy('favicon.ico');
@@ -20,7 +22,7 @@ module.exports = (eleventyConfig) => {
   eleventyConfig.setLibrary('md', markdownEngine);
 
   // Add filter for adding images in markdown
-  eleventyConfig.addLiquidFilter('markdownImage', function (imageFileName)  {
+  eleventyConfig.addLiquidFilter('markdownImage', function (imageFileName) {
     const fullPath = `../../assets/images/${imageFileName}`;
     return `[![](${fullPath}#markdown)](${fullPath})`;
   });
