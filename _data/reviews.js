45311ce0fbda323a5874fdd5f16a6b7e1d37703e
Arpit Batra <arpitbatra123@gmail.com>
2020-07-24 11:48:15 +0530

    try out goatcounter

--- _data/reviews.js
+++ _data/reviews.js
@@ -2,7 +2,7 @@ const Cache = require('@11ty/eleventy-cache-assets');
 
 module.exports = async function getReviews() {
   const response = await Cache('https://betterreads-seven.now.sh/api', {
-    duration: '1d', // 1 day
+    duration: '20d', // 20 days
     type: 'json'
   });
 
