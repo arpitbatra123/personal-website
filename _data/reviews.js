9c04f6b79060183df9a94cf92a2706a76316bee0
Arpit Batra <arpitbatra123@gmail.com>
2020-08-28 17:08:49 +0530

    add book cover for the year without pants

--- _data/reviews.js
+++ _data/reviews.js
@@ -1,7 +1,7 @@
 const Cache = require('@11ty/eleventy-cache-assets');
 
 module.exports = async function getReviews() {
-  const response = await Cache('https://betterreads-seven.now.sh/api', {
+  const response = await Cache('https://komura-api.vercel.app/api', {
     duration: '20d', // 20 days
     type: 'json'
   });
